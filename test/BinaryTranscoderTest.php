ab471352d458fd29dc796584221bafebb3ef6c39
Ryan Howe <43558896+ryanwhowe@users.noreply.github.com>
2018-10-17 12:39:30 -0400

    * Updating the README with additional information (#7)

--- test/BinaryTranscoderTest.php
+++ test/BinaryTranscoderTest.php
@@ -5,8 +5,9 @@
  * @author Ryan Howe
  * @since  2018-09-29
  */
-
 namespace Test;
+
+use ryanwhowe\BinaryTranscoder;
 
 class BinaryTranscoderTest extends \PHPUnit\Framework\TestCase
 {
@@ -76,6 +77,29 @@ class BinaryTranscoderTest extends \PHPUnit\Framework\TestCase
     }
 
     /**
+     * A dataProvider that has data in the form of $encodedStoredValue, $expectedDecodedValue .. These
+     * expectedDecodedValues and what are expected when the Padding_Boolean is set to null
+     *
+     * @return array
+     */
+    public function forwardTranscodingNullPadProvider()
+    {
+        return [
+            [ bindec('1000'), ['key1' => false, 'key2' => false, 'key3' => false, 'key4' => null] ],
+            [ bindec('1100'), ['key1' => true,  'key2' => false, 'key3' => false, 'key4' => null] ],
+            [ bindec('1010'), ['key1' => false, 'key2' => true,  'key3' => false, 'key4' => null] ],
+            [ bindec('1001'), ['key1' => false, 'key2' => false, 'key3' => true,  'key4' => null] ],
+            [ bindec('1110'), ['key1' => true,  'key2' => true,  'key3' => false, 'key4' => null] ],
+            [ bindec('1011'), ['key1' => false, 'key2' => true,  'key3' => true,  'key4' => null] ],
+            [ bindec('1111'), ['key1' => true,  'key2' => true,  'key3' => true,  'key4' => null] ],
+            [
+                bindec('1111'),
+                ['key1' => true,  'key2' => true,  'key3' => true,  'key4' => null, 'key5' => null, 'key6' => null]
+            ],
+        ];
+    }
+
+    /**
      * @test
      * @dataProvider transcodingProvider
      * @dataProvider forwardTranscodingFalsePadProvider
@@ -90,9 +114,14 @@ class BinaryTranscoderTest extends \PHPUnit\Framework\TestCase
      */
     public function decodingFalsePad($encoded_int, $expected_decode)
     {
-
-        $key_array = array_keys($expected_decode);
-        $tr = new \ryanwhowe\BinaryTranscoder($key_array);
+        $key_array = $expected_decode;
+        array_walk(
+            $key_array,
+            function (&$item) {
+                $item = null;
+            }
+        );
+        $tr = new BinaryTranscoder($expected_decode);
         $decode = $tr->decodeInteger($encoded_int);
 
         $this->assertEquals($expected_decode, $decode);
@@ -114,8 +143,43 @@ class BinaryTranscoderTest extends \PHPUnit\Framework\TestCase
     public function decodingTruePad($encoded_int, $expected_decode)
     {
 
-        $key_array = array_keys($expected_decode);
-        $tr = new \ryanwhowe\BinaryTranscoder($key_array, true);
+        $key_array = $expected_decode;
+        array_walk(
+            $key_array,
+            function (&$item) {
+                $item = null;
+            }
+        );
+        $tr = new BinaryTranscoder($key_array, BinaryTranscoder::BINARYTRANSCODER_PAD_TRUE);
+        $decode = $tr->decodeInteger($encoded_int);
+
+        $this->assertEquals($expected_decode, $decode);
+    }
+
+    /**
+     * @test
+     * @dataProvider transcodingProvider
+     * @dataProvider forwardTranscodingNullPadProvider
+     * @covers       \ryanwhowe\BinaryTranscoder::decodeInteger()
+     * @covers       \ryanwhowe\BinaryTranscoder::__construct()
+     * @covers       \ryanwhowe\BinaryTranscoder::convertProtectedIntToBin()
+     * @covers       \ryanwhowe\BinaryTranscoder::convertStringToArray()
+     * @covers       \ryanwhowe\BinaryTranscoder::determineMaxArrayLength()
+     * @param $encoded_int
+     * @param $expected_decode
+     * @throws \Exception
+     */
+    public function decodingNullPad($encoded_int, $expected_decode)
+    {
+
+        $key_array = $expected_decode;
+        array_walk(
+            $key_array,
+            function (&$item) {
+                $item = null;
+            }
+        );
+        $tr = new BinaryTranscoder($key_array, BinaryTranscoder::BINARYTRANSCODER_PAD_NULL);
         $decode = $tr->decodeInteger($encoded_int);
 
         $this->assertEquals($expected_decode, $decode);
@@ -136,7 +200,7 @@ class BinaryTranscoderTest extends \PHPUnit\Framework\TestCase
     public function encoding($expected_encode_int, $test_array)
     {
 
-        $tr = new \ryanwhowe\BinaryTranscoder($test_array);
+        $tr = new BinaryTranscoder($test_array);
         $encoded = $tr->encodeArray($test_array);
 
         $this->assertEquals($expected_encode_int, $encoded);
@@ -156,7 +220,7 @@ class BinaryTranscoderTest extends \PHPUnit\Framework\TestCase
         for ($i = 1; $i <= $array_count; $i++) {
             $test_array[$i] = false;
         }
-        new \ryanwhowe\BinaryTranscoder($test_array);
+        new BinaryTranscoder($test_array);
     }
 
     /**
@@ -174,7 +238,7 @@ class BinaryTranscoderTest extends \PHPUnit\Framework\TestCase
      */
     public function itemCountMissMatchEncoder($expected_encode_int, $test_array)
     {
-        $tr = new \ryanwhowe\BinaryTranscoder($test_array);
+        $tr = new BinaryTranscoder($test_array);
         array_push($test_array, array('key_z' => false));
         $tr->encodeArray($test_array);
     }
@@ -184,7 +248,7 @@ class BinaryTranscoderTest extends \PHPUnit\Framework\TestCase
      * @dataProvider             transcodingProvider
      * @dataProvider             forwardTranscodingFalsePadProvider
      * @expectedException \ryanwhowe\BinaryTranscoderException
-     * @expectedExceptionMessage The passed string does not have enough elements for the key array
+     * @expectedExceptionMessage The key array does not have enough elements for the decoded integer
      * @covers                   \ryanwhowe\BinaryTranscoder::decodeInteger()
      * @covers                   \ryanwhowe\BinaryTranscoder::__construct
      * @covers                   \ryanwhowe\BinaryTranscoder::convertProtectedIntToBin
@@ -197,7 +261,7 @@ class BinaryTranscoderTest extends \PHPUnit\Framework\TestCase
     public function itemCountMissMatchDecoder($encoded_int, $expected_decode)
     {
         $key_array = array_keys($expected_decode);
-        $tr = new \ryanwhowe\BinaryTranscoder($key_array);
+        $tr = new BinaryTranscoder($key_array);
         $failure_decode = bindec(str_pad('1', count($key_array) + 2, '0', STR_PAD_RIGHT));
         $decode = $tr->decodeInteger($failure_decode);
         $this->assertEquals($expected_decode, $decode);
